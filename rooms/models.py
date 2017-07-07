a5ddf4ec7d8a6c568188b08872f0265a90ce8ab3
Deneir Uy <deneiruy@gmail.com>
2017-07-07 17:17:32 +0800

    add User model

--- rooms/models.py
+++ rooms/models.py
@@ -1,7 +1,25 @@
 from django.db import models
 
+
 class Room(models.Model):
     beds = models.IntegerField()
+    room_number = models.IntegerField()
+    price = models.IntegerField()
 
     def __str__(self):
-        return "{}: {}".format(self.id, self.beds)
+        return "{}: {}".format(self.id, self.room_number)
+
+
+class User(models.Model):
+    username = models.CharField(max_length=20)
+
+    def __str__(self):
+        return self.username
+
+
+class Reservation(models.Model):
+    room = models.ForeignKey(Room, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return "{}: {}".format(self.room.id, self.room.room_number)
