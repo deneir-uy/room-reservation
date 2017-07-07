fc8b5d01b3c24f18963743ce34e19dd05f6f798c
Deneir Uy <deneiruy@gmail.com>
2017-07-07 17:21:00 +0800

    add User model

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,4 +1,6 @@
 from django.contrib import admin
-from .models import Room
+from .models import Room, Reservation, User
 
 admin.site.register(Room)
+admin.site.register(Reservation)
+admin.site.register(User)
