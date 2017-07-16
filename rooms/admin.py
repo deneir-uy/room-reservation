103522ad053c58bec68666f4d18c799c1ade58f6
Deneir Uy <deneiruy@gmail.com>
2017-07-16 16:18:57 +0800

    remove User

--- rooms/admin.py
+++ rooms/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Room, Reservation, User
+from .models import Room, Reservation
 
 admin.site.register(Room)
 admin.site.register(Reservation)
-admin.site.register(User)
